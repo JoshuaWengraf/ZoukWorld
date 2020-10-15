e08a351511d934c688575d748628af82a488a950
JoshuaWengraf <64872799+JoshuaWengraf@users.noreply.github.com>
2020-10-15 15:19:07 -0300

    Update talkmap.py

--- talkmap.py
+++ talkmap.py
@@ -40,8 +40,5 @@ for file in g:
 
 
 m = getorg.orgmap.create_map_obj()
-getorg.orgmap.output_html_cluster_map(location_dict, folder = "..", hashed_usernames=False)
-
-
-
+getorg.orgmap.output_html_cluster_map(location_dict, folder_name = "../talkmap", hashed_usernames=False)
 
